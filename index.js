66f4e750674508f567efb51e109a5e3ddf0149a9
Anette-99 <ascencio80998@gmail.com>
2021-06-04 12:43:12 -0500

    Use_SWAPI

---
+++ index.js
@@ -0,0 +1,44 @@
+//EJEMPLO DE JSONPLACEHOLDER
+
+// const API_URL = "https://jsonplaceholder.typicode.com";
+
+// const HTMLResponse = document.querySelector("#app");
+// const ul = document.createElement('ul');
+
+// fetch(`${API_URL}/comments`)
+//     .then((response) => response.json())
+//     .then((comments) => {
+//         comments.forEach(comment => {
+//             let elem = document.createElement('li');
+//             elem.appendChild(
+//                 document.createTextNode(`${comment.id} 📧 ${comment.email} -- ${comment.body}`)
+//             );
+//             ul.appendChild(elem);
+//         });
+//         HTMLResponse.appendChild(ul);
+//     });
+
+
+//EJEMPLO DE LA API DE STAR WARS
+
+const API_URL = "https://swapi.dev/api";
+
+const HTMLResponse = document.querySelector("#app");
+const ul = document.createElement('ul');
+
+fetch(`${API_URL}/planets/6/`)
+    .then((response) => response.json())
+    .then((planets) => {
+            let elem = document.createElement('li');
+            elem.appendChild(
+                document.createTextNode(` 🌍 ${planets.name} 🌥️ ${planets.climate} ⌛ ${planets.terrain} 🎥 ${planets.films}`)
+            );
+            ul.appendChild(elem);
+        });
+        HTMLResponse.appendChild(ul);
+
+// PARA OBTENER LA INFORMACION DESEADA EN CONSOLA
+
+// fetch('https://swapi.dev/api/planets/6/')
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
